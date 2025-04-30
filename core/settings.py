c834285a1a012ebb7d5981b56936099c6feba7d8
mjefagut01@tip.edu.ph <mjefagut01@tip.edu.ph>
2025-04-30 09:09:36 +0800

    allowed

--- core/settings.py
+++ core/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = os.getenv('DEBUG', '0').lower() in ['true','t','1']
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOST', '').split(' ')
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOST").split(' ')
 
 
 
